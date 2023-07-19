94cbac2949f2f55260b6b4e2a29fe0d64b970441
Sariuna <103085757+Sariuna@users.noreply.github.com>
2023-07-19 16:02:02 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import Profile from './components/profile/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dialogs from './components/dialogs/Dialogs';
+import News from './components/news/News';
 
-function App() {
+
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <div className='container'>
+          <div className="page">
+            <Navbar />
+            <div className='page_content'>
+              <Routes>
+                <Route exact path="/profile" element={<Profile />} />
+                <Route exact path="/dialogs" element={<Dialogs />} />
+                <Route exact path="/news" element={<News />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
